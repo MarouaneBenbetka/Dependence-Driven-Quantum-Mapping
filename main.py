48d99f19b2e46edbc2d589dde610844ff64e8ae4
MarouaneBenbetka <marouanebenbetka@gmail.com>
2024-10-10 01:29:31 +0100

    feat: preprocessing circuit polyhedral representation

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import islpy as isl
-from lib.graph_utils import *
-from lib.swap_utils import *
+from lib.graph_tools import *
+from lib.swap_tools import *
 
 def main():
+
+    logical_qubits_domain = isl.Set("{ q[i] : 1 <= i <  8 }")
+    physical_qubits_domain = isl.Set("{ Q[i] : 0 <= i <  8 }")
 
     graph = defaultdict(list)
     
@@ -15,8 +18,6 @@ def main():
     graph['Q[7]'] = ['Q[5]']
     graph['Q[8]'] = ['Q[6]']
 
-    logical_qubits_domain = isl.Set("{ q[i] : 1 <= i <  8 }")
-    physical_qubits_domain = isl.Set("{ Q[i] : 0 <= i <  8 }")
 
     shortest_paths = extract_shortest_paths(graph)
     swap_map = swaps_to_isl_map(shortest_paths['Q[2]']['paths']['Q[3]'])
@@ -24,8 +25,19 @@ def main():
     initial_mapping = isl.Map("{ q[i] -> Q[8-i] : 1<=i<8}")
     new_mapping = apply_swaps_to_logical_qubits_map(swap_map,initial_mapping,physical_qubits_domain)
 
-    print(new_mapping)
 
+    circuit_polyhedral_representation = json_file_to_isl("benchmarks/polyhedral/test.json")
+    domain = circuit_polyhedral_representation['domain']
+    schedule = circuit_polyhedral_representation['schedule']
+    read_dependencies = circuit_polyhedral_representation['read_dependencies']
+    
+    domain_with_multi_qubits_gates = extract_multi_qubit_gates(read_dependencies)
+    schedule_with_multi_qubits_gates = access_to_gates(read_dependencies,schedule.intersect_domain(domain_with_multi_qubits_gates))
+    read_dependencies_with_multi_qubits_ = read_dependencies.intersect_domain(domain_with_multi_qubits_gates)
+
+    
+    gates = access_to_gates(read_dependencies,schedule.intersect_domain(domain_with_multi_qubits_gates))
+    print(schedule.intersect_domain(domain_with_multi_qubits_gates))
 
 if __name__ == "__main__":
     main()
