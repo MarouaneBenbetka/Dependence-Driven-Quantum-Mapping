4de6c7c68567882057c2eebbf382a1e8753cd2ee
Marouane <MarouaneBenbetka@gmail.com>
2025-03-13 01:46:38 +0100

    feat: local backend topologies loading

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from qiskit.providers.fake_provider import Fake27QPulseV1, Fake5QV1, Fake20QV1
+from qiskit_ibm_runtime import QiskitRuntimeService
+
 from src.original_sabre.sabre import run_sabre
 from src.isl_routing.mapping.routing import POLY_QMAP
 from src.isl_routing.utils.isl_data_loader import *
 from src.isl_routing.utils.circuit_utils import *
+
+from src.isl_routing.backend.load_backend import load_backend_edges
 from time import time
 
 
 def run_single_file(file_path):
-    edges = Fake27QPulseV1().configuration().coupling_map
+    edges = load_backend_edges("ibm_sherbrooke")
 
     start = time()
     data = json_file_to_isl(file_path)
@@ -19,13 +23,10 @@ def run_single_file(file_path):
     poly_swap_count = poly_mapper.run(
         heuristic_method="decay", verbose=1)
 
-    single_trial_swap_count, multi_trial_swap_count = run_sabre(data, edges)
-    print(
-        f" poly_swap_with_transitive={poly_swap_count} ,single_trial_swap_count={single_trial_swap_count}, multi_trial_swap_count={multi_trial_swap_count}")
-
-    return poly_swap_count, single_trial_swap_count, multi_trial_swap_count
+    print(f"Poly Swap Count: {poly_swap_count}")
+    print(run_sabre(data, edges))
 
 
 if __name__ == "__main__":
     run_single_file(
-        fr"benchmarks/polyhedral/queko-bss-20qbt/20QBT_900CYC_QSE_9.json")
+        fr"benchmarks/polyhedral/queko-bss-16qbt/16QBT_700CYC_QSE_9.json")
